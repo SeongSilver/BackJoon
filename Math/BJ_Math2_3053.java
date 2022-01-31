9636fa327e649613a12404c9c38fb1d5349954fe
SeongSilver <95121282+SeongSilver@users.noreply.github.com>
2022-02-01 01:42:25 +0900

    Add files via upload

---
+++ Math/BJ_Math2_3053.java
@@ -0,0 +1,16 @@
+package Math;
+
+import java.util.Scanner;
+
+public class BJ_Math2_3053 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double R = sc.nextDouble();
+		
+		System.out.println(R*R*Math.PI);
+		System.out.println(2*R*R);
+	}
+
+}
