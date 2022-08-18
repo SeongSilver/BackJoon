15b5fea9b239148734e3b7eedf6e60778b58ad8e
SeongSilver <95121282+SeongSilver@users.noreply.github.com>
2022-08-18 12:14:02 +0900

    map배우고 다시 오자...

---
+++ JavaScript/1/18108.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const input = fs.readFileSync('/dev/stdin').toString();
+const A = parseInt(input[0]);
+const B = parseInt(input[1]);
+const C = parseInt(input[2]);
+const D = parseInt(input[3]);
+const E = parseInt(input[4]);
+const F = parseInt(input[5]);
+
+console.log(1-A);
+console.log(1-B);
+console.log(2-C);
+console.log(2-D);
+console.log(2-E);
+console.log(8-F);
+
+
